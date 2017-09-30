6d745bce2e491809263be04dbf6f824551ef783b
JordLoop <JordLoop@gmail.com>
2017-09-30 12:44:46 -0700

    add most basic services to game.service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class GameService {
@@ -7,7 +8,34 @@ export class GameService {
 
   constructor(private db: AngularFireDatabase) { }
 
+  saveGame(gameData) {
+    this.getAllGames().push(gameData);
+  }
 
+  getAllGames() {
+    return this.db.list('gameData/games');
+  }
 
+  getGameByKey(gameKey) {
+    return this.db.object(`gameData/games/${gameKey}`);
+  }
+     
+  saveRun(gameKey, runData) {
+    // let runToSave = {
+    //   "runData.gameKey": true,
+
+    // }
+    this.getRuns().push(runData);
+    this.getRunsByGameKey(gameKey).push(runData);
+  }
+
+  getRuns() {
+    return this.db.list('gameData/runs');
+  }
+  
+  //runsPerGame
+  getRunsByGameKey(gameKey) {
+    return this.db.list(`associationData/runsPerGame/${gameKey}`)
+  }  
 
 }
