542f6826e1db76da991808bb1cfc7b871b580fe4
JordLoop <JordLoop@gmail.com>
2017-10-24 13:00:20 -0700

    add components, nest components, and set up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,17 @@ import { GameService } from './services/game/game.service';
 import { PlayerService } from './services/player/player.service';
 import { RunService } from './services/run/run.service';
 //Components
-import { AppComponent } from '../components/app/app.component';
+import { AppComponent } from './app.component';
 import { GameSelectionComponent } from './components/game-selection/game-selection.component';
+import { ScorecardComponent } from './components/scorecard/scorecard.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
+import { PlayerListComponent } from './components/player-list/player-list.component';
+import { SessionSetupComponent } from './components/session-setup/session-setup.component';
+import { PlayerFormComponent } from './components/player-form/player-form.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { SessionDetailComponent } from './components/session-detail/session-detail.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -28,7 +37,16 @@ export const firebaseConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    GameSelectionComponent
+    GameSelectionComponent,
+    ScorecardComponent,
+    GameListComponent,
+    GameFormComponent,
+    PlayerDetailComponent,
+    PlayerListComponent,
+    SessionSetupComponent,
+    PlayerFormComponent,
+    GameDetailComponent,
+    SessionDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,22 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //import Components
 import { GameSelectionComponent } from './components/game-selection/game-selection.component';
+import { ScorecardComponent } from './components/scorecard/scorecard.component';
+import { SessionSetupComponent } from './components/session-setup/session-setup.component';
+
 
 const appRoutes: Routes = [
     {
-        path: '',
+        path: 'game_selection',
         component: GameSelectionComponent
+    },
+    {
+        path: 'scorecard',
+        component: ScorecardComponent
+    },
+    {
+        path: 'setup',
+        component: SessionSetupComponent
     }
 ];
 
