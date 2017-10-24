52c5d2a5b0e8f1090397d8fd79e683e94a50ccc1
JordLoop <JordLoop@gmail.com>
2017-10-24 15:31:52 -0700

    setup and test game.service.getGames

--- src/app/components/game-selection/game-selection.component.ts
+++ src/app/components/game-selection/game-selection.component.ts
@@ -7,15 +7,20 @@ import { GameService } from '../../services/game/game.service';
   styleUrls: ['./game-selection.component.css']
 })
 export class GameSelectionComponent implements OnInit {
-
+  games: any[];
   gameData: any = {
-    name: "Dominion",
+    name: "Carcassonne",
     sessionsCount: 0
   };
   
   constructor(private gameSvc: GameService) { }
 
   ngOnInit() {
+    // this.gameSvc.getGames().subscribe(games => {
+    //   this.games = games;
+    //   if(games)
+    //     console.log(this.games);  
+    // })
     // console.log('gameData component', this.gameData);    
     // this.gameSvc.saveGame(this.gameData);
   }
