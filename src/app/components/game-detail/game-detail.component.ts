3ff5d1b31f59481585236fcd60c275cd1562feb1
JordLoop <JordLoop@gmail.com>
2017-10-25 10:00:27 -0700

    displey content in game-detail based on key in route

--- src/app/components/game-detail/game-detail.component.ts
+++ src/app/components/game-detail/game-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { GameService } from '../../services/game/game.service';
+
 
 @Component({
   selector: 'app-game-detail',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameDetailComponent implements OnInit {
 
-  constructor() { }
+  gameKey: string = null;
+  game;
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private gameSvc: GameService
+  ) { }
 
   ngOnInit() {
+    this.getGameKeyViaRoute();
+    this.getGameByKey(this.gameKey);
+  }
+
+  getGameKeyViaRoute() {
+    this.route.params.forEach((param) => {
+      if(param)
+        this.gameKey = (param['id']);
+    });
+    console.log('onInit, gameKey:', this.gameKey);
+  }
+
+  getGameByKey(gameKey) {
+    this.gameSvc.getGameByKey(gameKey).subscribe(game => {
+      if (game)
+        this.game = game;
+      console.log(`onInit, game: ${gameKey}`, this.game);
+    });
   }
 
 }
