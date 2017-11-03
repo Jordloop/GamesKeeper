d985699fb3b4192f703b6c8ed8aa85fdae823a55
JordLoop <JordLoop@gmail.com>
2017-11-03 14:33:31 -0700

    clean up session.service

--- src/app/services/session/session.service.ts
+++ src/app/services/session/session.service.ts
@@ -44,6 +44,20 @@ export class SessionService {
     
     return this.db.list(`sessionData/sessions/${sessionKey}/players`);
   }
+  
+  getSessionByKey(sessionKey) {
+    return this.db.object(`sessionData/sessions/${sessionKey}`);
+  }
+  
+  incrementPlayerScore(sessionKey, player) {
+    const newScore = player.score + 1;
+    this.db.object(`sessionData/sessions/${sessionKey}/players/${player.$key}`).update({ score: newScore });
+  }
+  
+  decrementPlayerScore(sessionKey, player) {
+    const newScore = player.score - 1;
+    this.db.object(`sessionData/sessions/${sessionKey}/players/${player.$key}`).update({ score: newScore });
+  }
 
       //Works but is not used
   // getPlayersBySessionKey(sessionKey) {
@@ -55,85 +69,4 @@ export class SessionService {
   //       return Observable.combineLatest(firebaseObjectObservables)
   //     });
   // }
-
-  getSessionByKey(sessionKey) {
-    return this.db.object(`sessionData/sessions/${sessionKey}`);
-  }
-
-  incrementPlayerScore(sessionKey, player) {
-    const newScore = player.score + 1;
-    this.db.object(`sessionData/sessions/${sessionKey}/players/${player.$key}`).update({ score: newScore });
-  }
-
-  decrementPlayerScore(sessionKey, player) {
-    const newScore = player.score - 1;
-    this.db.object(`sessionData/sessions/${sessionKey}/players/${player.$key}`).update({ score: newScore });
-  }
-
-// -------------------------------------------------------
-
-
-  // createRunsPerGameAssociation() {
-
-  // }
-
-
-
-  // addRunKeyToGame(gameKey, runKey) {
-  //   this.db.object(`gameData/games/${gameKey}/runs/${runKey}`).set(true)
-
-  // }
-
-  // getPlayesForCurrentRun() {
-  //   return this.db.list(`associationData/playersPerRun/${this.currentRunKey}`);
-  // }
-
-
-  // getGameByRunKey(runKey) {
-  //   return this.db.object(`runData/runs/${runKey}`);
-  // }
-
-  // getRunsByPlayerKey(playerKey) {
-  //   const players = this.db.list(`associationData/runsPerPlayer/${playerKey}`);
-  //   return players;
-  // }
-
-  // getPlayersByRunKey(runKey: any) {
-  //   return this.db.list(`associationData/playersPerRun/${runKey}`);
-  // }
-  // updatePlayerInPlayersPerRun(runKey: any,
-  //   playerKey: any) {
-  //   return this.db.object(`associationData/playersPerRun/${runKey}/${playerKey}`);
-  // }
-  // updatePlayerInRunData(run, player) {
-  //   return this.db.object(`runData/runs/${run.$key}/players/${player.$key}`)
-  // }
-  // incrementScore(run, player) {
-  //   const newScore = player.score += 1;
-  //   this.updatePlayerInPlayersPerRun(run.$key, player.$key).update({ score: newScore })
-  //   this.updatePlayerInRunData(run, player).update({ score: newScore })
-  // }
-
-  // addPlayerToRun(runData, playerData) {
-  //   let playerToSave = {
-  //     playerKey: playerData.$key,
-  //     playerName: playerData.name,
-  //     score: 0
-  //   }
-  //   this.updatePlayerInRunData(runData.$key, playerData.$key).update(playerToSave)
-  //   this.updatePlayerInPlayersPerRun(runData.$key, playerData.$key).update(playerToSave);
-  //   // this.getRunsByPlayerKey(playerData.Key).push(runData);
-  // }
-
-  // navigateToRunDetail(runKey: any) {
-  //   this.currentRunKey = runKey;
-  //   this.currentRunKey$.next(runKey);
-  //   this.router.navigate([`run/${runKey}`]);
-  // }
-
-  // getMostRecentRunKey() {
-  //   this.getAllRuns();
-  // }
-
-
 }
