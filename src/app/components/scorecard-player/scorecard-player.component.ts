6ead5154f5e557729a020df3676ce2af218fe707
JordLoop <Jordloop@gmail.com>
2017-12-08 14:41:14 -0800

    move player content from scorecard component to scorecard-player component

--- src/app/components/scorecard-player/scorecard-player.component.ts
+++ src/app/components/scorecard-player/scorecard-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PlayerService } from '../../services/player/player.service';
 
 @Component({
   selector: 'app-scorecard-player',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scorecard-player.component.css']
 })
 export class ScorecardPlayerComponent implements OnInit {
+  @Input() player;
+  availablePlayers;
 
-  constructor() { }
+  constructor(public plrSvc: PlayerService) { }
 
   ngOnInit() {
+    this.getPlayers();
+  }
+
+  getPlayers() {
+    this.plrSvc.getPlayers().subscribe(players => {
+      if(players) {
+        this.availablePlayers = players;
+        console.log('getPlayers', this.availablePlayers);  
+      }
+    })
+  }
+
+  setPlayerName(player, value) {
+    player.name = value;
+    console.log('setPlayerName', player);
+    
   }
 
 }
--- src/app/components/scorecard/scorecard.component.ts
+++ src/app/components/scorecard/scorecard.component.ts
@@ -13,42 +13,31 @@ export class ScorecardComponent implements OnInit {
   constructor(public plrSvc: PlayerService) { }
 
   ngOnInit() {
-    this.getPlayers();
-  }
 
-  getPlayers() {
-    this.plrSvc.getPlayers().subscribe(players => {
-      if(players) {
-        this.availablePlayers = players;
-        console.log('getPlayers', this.availablePlayers);  
-      }
-    })
   }
 
   addPlayer() {
     const newPlayer = {name: "New Player", score: 0};
     this.players.push(newPlayer);
-  }
 
-  setPlayerName(player, value) {
-    player.name = value;
-    console.log('setPlayerName', player);
-    
   }
 
   incrementScore(player) {
     player.score++;
     console.log('incrementScore', player);
+
   }
 
   decrementScore(player) {
     player.score--;
     console.log('decrementScore', player);
+
   }
 
   adjustScore(player, event) {
     player.score = event.path[0].value;
     console.log('adjustScore', player);
+
   }
 
 }
